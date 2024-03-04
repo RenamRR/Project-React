079b0f0116617685b3dfdabb99ebe59cf15b4628
RenamRR <64269144+RenamRR@users.noreply.github.com>
2024-03-04 15:38:39 -0300

    Up <> Meu primeiro componente
    Criando meu componente e exportando e importando !

---
+++ 1_PROJECT/hello-world-2/src/components/Component.jsx
@@ -0,0 +1,11 @@
+const Component = () => {
+    // meu primeiro componente 
+    return(
+    <div>
+        {/* Meu primeiro componente*/}
+        <h1>Meu Primeiro componente!</h1>
+    </div>  
+    );
+};
+
+export default Component;
