c80f9a75f48cfe700c39e8fd94e2f557ebda2873
RenamRR <64269144+RenamRR@users.noreply.github.com>
2024-03-05 17:15:36 -0300

    Up <> States
    usando os states

--- 2_Project/ProjectRR/src/components/ListRender.jsx
+++ 2_Project/ProjectRR/src/components/ListRender.jsx
@@ -3,15 +3,36 @@ import { useState } from 'react'
 const ListRender = () => {
     const [list] = useState(['Thor','Renata','Renam','Nina']);
     
+    const [users, setUsers] = useState([
+        {id: 1, name:"Thor", age: 15},
+        {id: 2, name:"Renata", age: 59},
+        {id: 3, name:"Renam", age: 23},
+        {id: 4, name:"Nina", age: 10},
+    ]);
+
+    const deleteRandom = () =>{
+        const randomNumber = Math.floor(Math.random() * 4);
+
+        setUsers((prevUsers) => {
+            return prevUsers.filter((users) => randomNumber !== users.id)
+        });
+    };
+
     return (
         <div>
             <ul>
-                {list.map((item)=>(
-                    <li>{item}</li>
+                {list.map((item, i)=>(
+                    <li key={i}>{item}</li>
                 ))}
             </ul>
+            <ul>
+                {users.map((user) =>(
+                    <li key = {user.id}>{user.name}  tem {user.age} anos</li>
+                ))}
+            </ul>
+            <button onClick={deleteRandom}>Delete random</button>
         </div>
     )
-}
+};
 
 export default ListRender
